99e9808e9a825c47099766c1749aa063f4c607dc
nttr <nttr@BI-23564.local>
2017-08-02 14:27:33 +0900

    カウントアプリの基本機能学習

--- Count_20170802/ViewController.swift
+++ Count_20170802/ViewController.swift
@@ -1,6 +1,6 @@
 //
 //  ViewController.swift
-//  Count_20170802
+//  170802_count
 //
 //  Created by nttr on 2017/08/02.
 //  Copyright © 2017年 nttr. All rights reserved.
@@ -9,17 +9,27 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    var number: Int = 0
+    @IBOutlet var label: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
-
-
+    
+    @IBAction func plus(){
+        number = number + 1
+        label.text = String(number)
+    }
+    
+    @IBAction func minus(){
+        number = number - 1
+        label.text = String(number)
+    }
+    
+    
 }
 
